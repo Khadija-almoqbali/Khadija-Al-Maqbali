301b149a5b3e59f2fe1896b34db87aaec55ef995
Khadija Al-Maqbali <khadija@Khadijas-MacBook-Air.local>
2026-06-09 10:29:42 +0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a brief introduction about myself.</p>
+    </div>
+  )
+}
+
+export default App
